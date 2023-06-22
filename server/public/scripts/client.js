4a102d373b618b8121e3c35246cd959701a2fdb0
Peder Goodman <peder.goodman@gmail.com>
2023-06-22 12:25:55 -0500

    added git route on server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,7 +30,7 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+
 } // end getKoalas
 
 function saveKoala( newKoala ){
--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -6,7 +6,19 @@ const pool = require('../modules/pool');
 
 
 // GET
+router.get('/', (req, res) => {
+  let queryText = `SELECT * FROM "kolas"`
 
+  pool.query(queryText)
+    .then((result) => {
+      // send table row data
+      res.send(result.rows)
+    }).catch((err) => {
+      // error if trouble getting koala list
+      console.log('Error getting koala list', err);
+      res.sendStatus(500)
+    });
+});
 
 // POST
 
