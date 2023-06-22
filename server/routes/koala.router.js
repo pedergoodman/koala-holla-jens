7dd4963db00967cdc308ef00e000253d1b5fe930
Savon <savonstephens4@gmail.com>
2023-06-22 12:08:30 -0700

    declared data for the input fields

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -8,7 +8,17 @@ const koalaRouter = express.Router();
 
 
 // POST
+app.post('/koala', (req, res) => {
+    //log to see if post is working 
+console.log('inside of koalas 🐨, req.body', req.body);
+let id = req.body.id
+let name = req.body.name 
+let gender = req.body.gender 
+let age = req.body.age
+let ready_to_transfer = req.body.ready_to_transfer
 
+
+})
 
 // PUT
 
