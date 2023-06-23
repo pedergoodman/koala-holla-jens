96931ddd3cfb3b44b550b7acd1c0b04ec724ee01
Savon <savonstephens4@gmail.com>
2023-06-23 10:05:45 -0700

    just finished the delete router/server side

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -7,19 +7,19 @@ const pool = require('../modules/pool');
 
 // GET
 koalaRouter.get('/', (req, res) => {
-  console.log('in /koala GET');
+    console.log('in /koala GET');
+    ``
+    let queryText = `SELECT * FROM "koala"`
 
-  let queryText = `SELECT * FROM "koala"`
-
-  pool.query(queryText)
-    .then((result) => {
-      // send table row data
-      res.send(result.rows)
-    }).catch((err) => {
-      // error if trouble getting koala list
-      console.log('Error getting koala list', err);
-      res.sendStatus(500)
-    });
+    pool.query(queryText)
+        .then((result) => {
+            // send table row data
+            res.send(result.rows)
+        }).catch((err) => {
+            // error if trouble getting koala list
+            console.log('Error getting koala list', err);
+            res.sendStatus(500)
+        });
 });
 
 // POST
@@ -51,16 +51,31 @@ koalaRouter.put('/:id', (req, res) => {
     const idToUpdate = req.params.id;
     let query = `UPDATE "koala" SET "ready_to_transfer"=true WHERE "id"=$1`;
     pool.query(query, [idToUpdate])
-    .then((result) => {
-        console.log('Koala updated!');
-        res.sendStatus(200);
-    })
-    .catch((error) => {
-        console.log('Error making update request: ', error);
-        res.sendStatus(500);
-    })
+        .then((result) => {
+            console.log('Koala updated!');
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('Error making update request: ', error);
+            res.sendStatus(500);
+        })
 })
 
 // DELETE
+Router.delete('/:id', (req, res) => {
+    idToDelete = req.params.id
+    //query text to delete koalas by id and to protect against sql injection
+    let queryText = `DELETE FROM "koala" WHERE id = $1;`
+    //connecting with the data base and running our query
+    pool.query(queryText, [idToDelete])
+        .then((results) => {
+            console.log('koala deleted');
+            //sending the ok status the the koala 
+            res.sendStatus(200)
+        }).catch((error) => {
+            console.log('error making data base query', error);
+            res.sendStatus(500)
+        })
+})
 
 module.exports = koalaRouter;
